7cfe0b9eda2cfd930db1e4e1799c9cb3f7752d9b
Ashley Strandberg <ashley.strandberg808@gmail.com>
2021-12-10 23:52:02 -1000

    added enemy combats

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,13 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames.length);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 // Window Alert to notify players they start playing the game //
   window.alert("Welcome to Robot Gladiators!");
@@ -62,4 +64,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 }
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
